b03c7bcfef21d2f1dafffbbad66cfbbe37f9e36e
Dino <104674307+poljak100@users.noreply.github.com>
2022-10-01 21:45:53 +0200

    Napravljen Osoba Controller

--- www/rad.hr/app/controller/OsobaController.php
+++ www/rad.hr/app/controller/OsobaController.php
@@ -4,132 +4,91 @@ class OsobaController extends AutorizacijaController
 {
 
     private $phtmlDir = 'privatno' .
-        DIRECTORY_SEPARATOR . 'osoba' .
+        DIRECTORY_SEPARATOR . 'osobe' .
         DIRECTORY_SEPARATOR;
 
-    private $osoba = null;
+    private $entitet = null;
     private $poruka = '';
 
     public function index()
     {
-        $this->view->render($this->phtmlDir . 'read', [
-            'osoba' => Osoba::read()
+        $this->view->render($this->phtmlDir . 'index', [
+            'entiteti' => Osoba::read()
         ]);
-    }
-
-    public function promjena($sifra)
-    {
-        if (!isset($_POST['naziv'])) {
-
-            $osoba = Osoba::readOne($sifra);
-            if ($osoba == null) {
-                header('location: ' . App::config('url') . 'osoba');
-            }
-
-            $this->view->render($this->phtmlDir . 'update', [
-                'osoba' => $osoba,
-                'poruka' => 'Promjenite podatke'
-            ]);
-            return;
-        }
-
-        $this->osoba = (object) $_POST;
-        $this->osoba->sifra = $sifra;
-
-
-        if ($this->kontrolaPromjena()) {
-            Osoba::update((array)$this->osoba);
-            //header('location: ' . App::config('url') . 'osoba');
-            return;
-        }
-
-        $this->view->render($this->phtmlDir . 'update', [
-            'osoba' => $this->osoba,
-            'poruka' => $this->poruka
-        ]);
-    }
-
-    public function brisanje($sifra)
-    {
-
-        $osoba = Osoba::readOne($sifra);
-        if ($osoba == null) {
-            header('location: ' . App::config('url') . 'osoba');
-        }
-
-        //  if (!isset($_POST['obrisi'])) {
-        //      $this->view->render($this->phtmlDir . 'delete', [
-        //          'osoba' => $osoba,
-        //          'brisanje' => Osoba::brisanje($sifra),
-        //          'poruka' => 'Detalji osobe za brisanje'
-        //      ]);
-        //      return;
-        //  }
-
-        Osoba::delete($sifra);
-        header('location: ' . App::config('url') . 'osoba');
     }
 
     public function novi()
     {
-        if (!isset($_POST['naziv'])) {
+        $novaOsoba = Osoba::create([
+            'ime' => '',
+            'prezime' => '',
+            'mjesto_stanovanja' => '',
+            'oib' => '',
+            'datum_rodenja' => '',
+            'naziv_terena' => '',
+            'smjena' => '',
+            'email' => ''
+        ]);
+        header('location: ' . App::config('url')
+            . 'osoba/promjena/' . $novaOsoba);
+    }
 
-            $this->PripremiOsoba();
-            $this->view->render($this->phtmlDir . 'create', [
-                'osoba' => $this->osoba,
-                'poruka' => 'Popunite sve podatke'
+    public function promjena($sifra)
+    {
+        if (!isset($_POST['ime'])) {
+
+            $e = Osoba::readOne($sifra);
+            if ($e == null) {
+                header('location: ' . App::config('url') . 'osoba');
+            }
+
+            $this->view->render($this->phtmlDir . 'detalji', [
+                'e' => $e,
+                'poruka' => 'Unesite podatke'
             ]);
             return;
         }
-        $this->osoba = (object) $_POST;
 
+        $this->entitet = (object) $_POST;
+        $this->entitet->sifra = $sifra;
 
-        if ($this->kontrolaNovi()) {
-            Osoba::create((array)$this->osoba);
+        if ($this->kontrola()) {
+            Osoba::update((array)$this->entitet);
             header('location: ' . App::config('url') . 'osoba');
             return;
         }
 
-        $this->view->render($this->phtmlDir . 'create', [
-            'osoba' => $this->osoba,
+        $this->view->render($this->phtmlDir . 'detalji', [
+            'e' => $this->entitet,
             'poruka' => $this->poruka
         ]);
     }
-    // NE
-    private function kontrolaNovi()
+
+    private function kontrola()
     {
-        return $this->kontrolaNaziv();
+        return $this->kontrolirajIme() && $this->kontrolirajPrezime() && $this->kontrolirajOib();
     }
 
-    private function kontrolaPromjena()
+    private function kontrolirajIme()
     {
-        return $this->kontrolaNaziv();
-    }
-
-    private function kontrolaNaziv()
-    {
-        Log::log($this->osoba->naziv);
-        $this->osoba->naziv = str_replace('&nbsp;', ' ', $this->osoba->naziv);
-        $this->osoba->naziv = trim($this->osoba->naziv);
-        Log::log($this->osoba->naziv);
-        if (strlen($this->osoba->naziv) === 0) {
-            $this->poruka = 'Naziv obavezno';
-            return false;
-        }
         return true;
     }
 
-
-    private function PripremiOsoba()
+    private function kontrolirajPrezime()
     {
-        $this->osoba = new stdClass();
-        $this->osoba->ime = '';
-        $this->osoba->prezime = '';
-        $this->osoba->mjesto_stanovanja = '';
-        $this->osoba->oib = '';
-        $this->osoba->datum_rodenja = '';
-        $this->osoba->naziv_terena = '';
-        $this->osoba->smjena = '';
-        $this->osoba->email = '';
+
+        return true;
+    }
+
+    private function kontrolirajOib()
+    {
+
+        return true;
+    }
+
+    public function brisanje($sifra)
+    {
+        Osoba::delete($sifra);
+        header('location: ' . App::config('url') . 'osoba');
     }
 }
