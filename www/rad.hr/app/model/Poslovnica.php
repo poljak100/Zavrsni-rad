5410117e53c50cea43f4f08364f30663f074dc64
Dino <104674307+poljak100@users.noreply.github.com>
2022-10-01 21:46:26 +0200

    model Poslovnica

--- www/rad.hr/app/model/Poslovnica.php
+++ www/rad.hr/app/model/Poslovnica.php
@@ -1,37 +1,41 @@
 <?php
 
-class Poslovnica
-{
-
-    public static function readOne($sifra)
+    class Poslovnica
     {
-        $veza = DB::getInstance();
-        $izraz = $veza->prepare('
+
+        public static function brisanje($sifra)
+        {
+            $veza = DB::getInstance();
+            $izraz = $veza->prepare('
+        
+            delete from poslovnica where sifra=:sifra
+        
+        ');
+            $izraz->execute([
+                'sifra' => $sifra
+            ]);
+            $izraz->fetchColumn();
+        }
+
+        public static function readOne($sifra)
+        {
+            $veza = DB::getInstance();
+            $izraz = $veza->prepare('
         
             select * from poslovnica where sifra=:sifra
         
         ');
-        $izraz->execute([
-            'sifra' => $sifra
-        ]);
-        return $izraz->fetch();
-    }
+            $izraz->execute([
+                'sifra' => $sifra
+            ]);
+            return $izraz->fetch();
+        }
 
-    public static function read()
-    {
-        $veza = DB::getInstance();
-        $izraz = $veza->prepare('
-        
-            select * from poslovnica
-        
-        ');
-        $izraz->execute();
-        return $izraz->fetchAll();
-    }
 
     public static function create($poslovnica)
     {
         $veza = DB::getInstance();
+
         $izraz = $veza->prepare('
         
             insert into 
@@ -40,34 +44,51 @@ class Poslovnica
         
         ');
         $izraz->execute($poslovnica);
+        return $veza->lastInsertId();
     }
 
-    public static function update($poslovnica)
-    {
 
-        $veza = DB::getInstance();
-        $izraz = $veza->prepare('
+        
+        public static function read()
+        {
+            $veza = DB::getInstance();
+            $izraz = $veza->prepare('
+        
+            select * from poslovnica 
+        
+        ');
+            $izraz->execute();
+            return $izraz->fetchAll();
+        }
+
+        
+        
+        public static function update($poslovnica)
+        {
+            $veza = DB::getInstance();
+            $izraz = $veza->prepare('
         
             update poslovnica set
-            naziv=:naziv,
+                naziv=:naziv,
                 mjesto=:mjesto,
-                email=:email,
+                email=:email
                 where sifra=:sifra
         
         ');
-        $izraz->execute($poslovnica);
-    }
+            $izraz->execute($poslovnica);
+        }
 
-    public static function delete($sifra)
-    {
-        $veza = DB::getInstance();
-        $izraz = $veza->prepare('
         
-            delete from poslovnica where sifra=:sifra 
-    
+        public static function delete($sifra)
+        {
+            $veza = DB::getInstance();
+            $izraz = $veza->prepare('
+        
+            delete from poslovnica where sifra=:sifra
+        
         ');
-        $izraz->execute([
-            'sifra' => $sifra
-        ]);
+            $izraz->execute([
+                'sifra' => $sifra
+            ]);
+        }
     }
-}
