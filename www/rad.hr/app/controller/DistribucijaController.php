58d2664370454052f9ffa7513e86494c79f5fa3d
Dino <104674307+poljak100@users.noreply.github.com>
2022-10-03 10:44:03 +0200

    Distribucija

--- www/rad.hr/app/controller/DistribucijaController.php
+++ www/rad.hr/app/controller/DistribucijaController.php
@@ -2,58 +2,91 @@
 
 class DistribucijaController extends AutorizacijaController
 {
-    private $phtmlDir = 'privatno' . 
-        DIRECTORY_SEPARATOR . 'distribucija' .
+
+    private $phtmlDir = 'privatno' .
+        DIRECTORY_SEPARATOR . 'distribucije' .
         DIRECTORY_SEPARATOR;
 
-    private $entitet;
-    private $poruka;
+    private $entitet = null;
+    private $poruka = '';
 
     public function index()
     {
-        $this->view->render($this->phtmlDir . 'index',[
+        $this->view->render($this->phtmlDir . 'index', [
             'entiteti' => Distribucija::read()
         ]);
     }
 
-    public function nova()
+    public function novi()
     {
-        $novi = Distribucija::create([
-            'mjesto'=>'',
-            'vrijeme'=>'',
-            'kolicina'=>'',
-            'osoba'=>''
-            
+        $novaDistribucija = Distribucija::create([
+            'naziv' => '',
+            'mjesto' => '',
+            'email' => ''
         ]);
-        header('location: ' . App::config('url') 
-                . 'distribucija/promjena/' . $novi);
+        header('location: ' . App::config('url')
+            . 'distribucija/promjena/' . $novaDistribucija);
     }
 
     public function promjena($sifra)
     {
-        if(!isset($_POST['naziv'])){
+        if (!isset($_POST['naziv'])) {
 
             $e = Distribucija::readOne($sifra);
-            if($e==null){
+            if ($e == null) {
                 header('location: ' . App::config('url') . 'distribucija');
             }
 
-            $this->view->render($this->phtmlDir . 'detalji',[
+            $this->view->render($this->phtmlDir . 'detalji', [
                 'e' => $e,
-                'osoba'=>Osoba::read(),
                 'poruka' => 'Unesite podatke'
             ]);
             return;
         }
 
         $this->entitet = (object) $_POST;
-        $this->entitet->sifra=$sifra;
-    
+        $this->entitet->sifra = $sifra;
 
-        $this->view->render($this->phtmlDir . 'detalji',[
-            'e'=>$this->entitet,
-            'osoba'=>Osoba::read(),
-            'poruka'=>$this->poruka
+        if ($this->kontrola()) {
+            Distribucija::update((array)$this->entitet);
+            header('location: ' . App::config('url') . 'distribucija');
+            return;
+        }
+
+        $this->view->render($this->phtmlDir . 'detalji', [
+            'e' => $this->entitet,
+            'poruka' => $this->poruka
         ]);
+    }
+
+    private function kontrola()
+    {
+        return  $this->kontrolirajMjesto()
+            && $this->kontrolirajVrijeme()
+            && $this->kontrolirajKolicina();
+    }
+
+    private function kontrolirajVrijeme()
+    {
+        return true;
+    }
+
+    private function kontrolirajKolicina()
+    {
+
+        return true;
+    }
+
+    private function kontrolirajMjesto()
+    {
+
+        return true;
+    }
+
+
+    public function brisanje($sifra)
+    {
+        Distribucija::delete($sifra);
+        header('location: ' . App::config('url') . 'distribucija');
     }
 }
--- www/rad.hr/app/model/Distribucija.php
+++ www/rad.hr/app/model/Distribucija.php
@@ -3,78 +3,36 @@
 class Distribucija
 {
 
-
-    public static function readOne($sifra)
-    {
-        $veza = DB::getInstance();
-        $izraz = $veza->prepare('
-        
-           select * from distribucija where sifra=:sifra
-        
-        ');
-        $izraz->execute([
-            'sifra' => $sifra
-        ]);
-        $distribucija = $izraz->fetch();
-
-        $izraz = $veza->prepare('
-        
-                select  a.ime, a.prezime ,a.naziv_terena  ,b.mjesto , b.vrijeme , b.kolicina 
-                from osoba  a inner join distribucija  b
-                on a.sifra =b.osoba where sifra=:sifra
-        
-        ');
-        $izraz->execute([
-            'sifra' => $sifra
-        ]);
-        $distribucija->osoba = $izraz->fetchAll();
-
-        return $distribucija;
-    }
-
-    public static function read()
-    {
-        $veza = DB::getInstance();
-        $izraz = $veza->prepare('
-        
-         
-        ');
-        $izraz->execute();
-        return $izraz->fetchAll();
-    }
-
-    public static function create($p)
-    {
-
-        $veza = DB::getInstance();
-        $izraz = $veza->prepare('
-        
-        insert into distribucija
-            (naziv,datumpocetka,maksimalnopolaznika,
-            smjer,predavac)
-            values
-            (:naziv,:datumpocetka,:maksimalnopolaznika,
-            :smjer,:predavac);
-        
-        ');
-        $izraz->execute($p);
-        return $veza->lastInsertId();
-    }
-
-    public static function update($p)
-    {
-    }
-
     public static function delete($sifra)
     {
         $veza = DB::getInstance();
         $izraz = $veza->prepare('
         
-        delete from distribucija where sifra=:sifra 
+            delete from distribucija where sifra=:sifra
         
         ');
         $izraz->execute([
-            'sifra' => $sifra
+            'sifra'=>$sifra
         ]);
+        $izraz->fetchColumn();
     }
+
+    public static function readOne($sifra)
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare('
+            select a.mjesto ,a.vrijeme ,a.kolicina ,b.ime ,b.prezime , b.oib 
+            from distribucija a inner join osoba b 
+            on a.osoba =b.sifra 
+            where a.sifra=:sifra
+        
+        ');
+        $izraz->execute([
+            'sifra'=>$sifra
+        ]);
+        return $izraz->fetch(); 
+    }
+
+
+
 }
